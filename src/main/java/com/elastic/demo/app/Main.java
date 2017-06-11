d746808db8f72f208d45bf5b7f9bb5b75d5b9b9a
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-06-11 19:09:29 +0530

    update functionality

--- src/main/java/com/elastic/demo/app/Main.java
+++ src/main/java/com/elastic/demo/app/Main.java
@@ -1,9 +1,7 @@
 package com.elastic.demo.app;
 
-import com.elastic.demo.hotel.Supplier;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -26,15 +24,9 @@ public class Main
             client.addTransportAddress( new InetSocketTransportAddress( InetAddress.getLocalHost(), 9200 ) );
 
             ObjectMapper mapper = new ObjectMapper();
-            byte [] json = mapper.writeValueAsBytes( new Supplier() );
-            System.out.println(json.toString());
 
         }
         catch( UnknownHostException e )
-        {
-            e.printStackTrace();
-        }
-        catch( JsonProcessingException e )
         {
             e.printStackTrace();
         }
--- src/main/java/com/elastic/demo/books/BookUtil.java
+++
@@ -1,9 +0,0 @@
-package com.elastic.demo.books;
-
-/**
- * Created by Kinath on 6/8/2017.
- */
-public class BookUtil
-{
-    
-}
